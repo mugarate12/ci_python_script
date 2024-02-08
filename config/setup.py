a9ae7649a4448639916b69a2eb1081d40953caaa
Mateus Cardoso <serjumano17@gmail.com>
2024-02-07 21:16:31 -0300

    feat: :sparkles: add a simple connection of mysql database

--- config/setup.py
+++ config/setup.py
@@ -7,3 +7,8 @@ path = os.path.join(config_dir, '..', '.env')
 load_dotenv(path)
 
 NAME = os.getenv('NAME')
+
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-from config.setup import NAME
+import mysql.connector
+from config.setup import NAME, DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT
 
 if __name__ == '__main__':
+    try:
+        conn = mysql.connector.connect(
+            user=DATABASE_USER,
+            password=DATABASE_PASSWORD,
+            host=DATABASE_HOST,
+            port=DATABASE_PORT
+        )
+    except mysql.connector.Error as err:
+        print(err)
+        exit(1)
+    finally:
+        cursor = conn.cursor()
+        conn = conn
+        print('conectei com o banco de dados!')
+
     print('Seu nome informado na .env é: ')
     print(NAME)
